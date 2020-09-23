98aeff37b2fad8c203480d7ea65b6566a82c6c8f
Pablo Groba <pgroba@gmail.com>
2020-09-23 19:58:51 -0300

    Add Docs folder

---
+++ docs/precache-manifest.2b1b82c0b3e89db6a55138ab20ba5807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2d3d0151f5c16c0ddf075fcadfa251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2996448608caccd76310",
+    "url": "/static/css/main.37060361.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2996448608caccd76310",
+    "url": "/static/js/main.9754b346.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
